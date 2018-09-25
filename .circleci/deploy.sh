986d2fd5c69a38a772df1c4cb5a0fb9e668d2f10
Dian <dian@petanikode.com>
2018-09-26 03:26:21 +0800

    update script

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,4 +1,5 @@
-git clone --depth=1 https://github.com/petanikode/tutorial-codeigniter.git app && cd app
+git clone --depth=1 https://github.com/petanikode/tutorial-codeigniter.git app
+cd app
 git config git-ftp.url $FTP_HOST
 git config git-ftp.user $FTP_USER
 git config git-ftp.password $FTP_PASS
