da28c4ce7e946487bab6f50f1becaab8d65f61eb
Dian <dian@petanikode.com>
2018-09-26 05:03:53 +0800

    update config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/tokobuah';
+$config['base_url'] = (ENVIRONMENT !== 'production') ? 'http://localhost/tokobuah' : $_SERVER['SERVER_ADDR'];
 
 /*
 |--------------------------------------------------------------------------
--- application/config/database.php
+++ application/config/database.php
@@ -70,7 +70,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-$active_group = 'default';
+$active_group = (ENVIRONMENT !== 'production') ? 'default':'production';
 $query_builder = TRUE;
 
 $db['default'] = array(
@@ -80,6 +80,28 @@ $db['default'] = array(
 	'password' => 'kopi',
 	'database' => 'tokobuah',
 	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
+
+$db['production'] = array(
+	'dsn'	=> '',
+	'hostname' => 'sql302.epizy.com',
+	'username' => 'epiz_22757543',
+	'password' => 'd6LX6shPkijKLi',
+	'database' => 'tokobuah',
+	'dbdriver' => 'epiz_22757543_tokobuah',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
