e36267df1ddfb1aca12b45d09b3592139d195f2a
Dian <dian@petanikode.com>
2018-09-25 16:15:55 +0800

    created _uploadImage() method

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -42,7 +42,8 @@ class Product_model extends CI_Model
         $post = $this->input->post();
         $this->product_id = uniqid();
         $this->name = $post["name"];
-        $this->price = $post["price"];
+		$this->price = $post["price"];
+		$this->image = $this->_uploadImage();
         $this->description = $post["description"];
         $this->db->insert($this->_table, $this);
     }
@@ -52,7 +53,14 @@ class Product_model extends CI_Model
         $post = $this->input->post();
         $this->product_id = $post["id"];
         $this->name = $post["name"];
-        $this->price = $post["price"];
+		$this->price = $post["price"];
+		
+		if ($_FILES["image"]) {
+            $this->image = $this->_uploadImage();
+        } else {
+            $this->image = $post["old_image"];
+		}
+		
         $this->description = $post["description"];
         $this->db->update($this->_table, $this, array('product_id' => $post['id']));
     }
@@ -60,5 +68,23 @@ class Product_model extends CI_Model
     public function delete($id)
     {
         return $this->db->delete($this->_table, array("product_id" => $id));
-    }
+	}
+	
+	private function _uploadImage()
+	{
+		$config['upload_path']          = './upload/product/';
+		$config['allowed_types']        = 'gif|jpg|png';
+		$config['file_name']            = $this->product_id;
+		$config['max_size']             = 1024; // 1MB
+		// $config['max_width']            = 1024;
+		// $config['max_height']           = 768;
+
+		$this->load->library('upload', $config);
+
+		if ($this->upload->do_upload('image')) {
+			return $this->upload->data("file_name");
+		}
+		
+		return "default.jpg";
+	}
 }
