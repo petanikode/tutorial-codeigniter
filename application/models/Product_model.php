27d1226abeace2272431cb7ce82644e0d43f45bf
Dian <dian@petanikode.com>
2018-09-25 17:41:57 +0800

    screenshot done

--- application/models/Product_model.php
+++ application/models/Product_model.php
@@ -71,5 +71,24 @@ class Product_model extends CI_Model
         return $this->db->delete($this->_table, array("product_id" => $id));
 	}
 	
+	
+	private function _uploadImage()
+	{
+		$config['upload_path']          = './upload/product/';
+		$config['allowed_types']        = 'gif|jpg|png';
+		$config['file_name']            = $this->product_id;
+		$config['overwrite']			= true;
+		$config['max_size']             = 1024; // 1MB
+		// $config['max_width']            = 1024;
+		// $config['max_height']           = 768;
+
+		$this->load->library('upload', $config);
+
+		if ($this->upload->do_upload('image')) {
+			return $this->upload->data("file_name");
+		}
+		
+		return "default.jpg";
+	}
 
 }
